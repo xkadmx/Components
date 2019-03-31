0599edb4f5c1c8325a7562c38540c87ebe7bd6b2
xkadmx <zuzanna.rosinska@yahoo.com>
2019-03-31 15:13:12 +0200

    creating customDialog method in MainActivity class (instead of createDialog used previously) to set content view in a different layout (dialog_test) containing textview and a return button.
    Signed-off-by: xkadmx <zuzanna.rosinska@yahoo.com>

--- app/src/main/java/com/example/components/MainActivity.java
+++ app/src/main/java/com/example/components/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.components;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
@@ -15,6 +18,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //createDialog();
+        createCustomDialog();
+
+
+    }
+    public void createDialog() {
         AlertDialog dialog = new AlertDialog.Builder(this)
                 .setTitle("Niedzielny serwis dezinformacyjny:")
                 .setMessage("Słowo na niedzielę")
@@ -30,7 +39,25 @@ public class MainActivity extends Activity {
                     public void onClick(DialogInterface dialog, int which) {
                         Toast.makeText(MainActivity.this,"Próbuj!",Toast.LENGTH_SHORT).show();
 
+
                     }
                 }).show();
     }
-}
+    public void createCustomDialog(){
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_test);
+        dialog.setTitle("Test");
+
+        Button exit = (Button) dialog.findViewById(R.id.buttonExit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+    }
+
+    }
+
